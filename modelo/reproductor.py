de504ca9ea68d959ab004f314eccaa45ae43cb86
crisspro <62963843+crisspro@users.noreply.github.com>
2022-08-16 19:51:40 -0400

    Agregado reproductor al modelo

---
+++ modelo/reproductor.py
@@ -0,0 +1,22 @@
+﻿class Reproductor():
+	def __init__(self):
+		self.estado = 'stop'
+		self.pista = None
+		self.tiempo_actual = 0
+		self.volumen = 100
+
+
+	def cargar(self, pista):
+		self.pista = pista
+
+	def refrescar(self):
+		self.__init__()
+
+	def reproducir(self):
+		self.estado = 'play'
+
+	def pausar(self):
+		self.estado = 'pause'
+
+	def detener(self):
+		self.estado = 'stop'
